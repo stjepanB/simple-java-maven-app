ffebdd71c1fb23f05940306335238e71cc75e8e1
stjepanB <stjepan.bencic95@gmail.com>
2023-10-19 09:09:26 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,6 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
+mvn clean install | sed -r "s/\x1B\[[0-9;]*[JKmsu]//g"
 java -jar target/"${NAME}-${VERSION}.jar"
 
